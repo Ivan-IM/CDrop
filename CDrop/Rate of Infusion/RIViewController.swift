190429248913979257c106f9933a29f138b83eec
iMarishin <move_up@icloud.com>
2021-06-19 16:35:28 +0500

    add keyboard

--- CDrop/Rate of Infusion/RIViewController.swift
+++ CDrop/Rate of Infusion/RIViewController.swift
@@ -15,11 +15,15 @@ class RIViewController: UIViewController {
     
     private var rof = RateOfInfusion(volume: 0.0, time: 0, speed: 0.0)
     
+    @objc private func didTapDone() {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        volumeTextField.text = String(rof.volume)
-        timeTextField.text = String(rof.time)
-        speedTextField.text = String(rof.speed)
+        volumeTextField.text = nil
+        timeTextField.text = nil
+        speedTextField.text = nil
         
         volumeTextField.delegate = self
         timeTextField.delegate = self
@@ -37,5 +41,31 @@ extension RIViewController: UITextFieldDelegate {
             volume: Float(volumeTextField.text ?? "0.0") ?? 0.0,
             time: Int(timeTextField.text ?? "0") ?? 0,
             speed: Float(speedTextField.text ?? "0.0") ?? 0.0)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let keyboardToolbar = UIToolbar()
+        textField.inputAccessoryView = keyboardToolbar
+        keyboardToolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(
+            title:"Done",
+            style: .done,
+            target: self,
+            action: #selector(didTapDone)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
     }
 }
